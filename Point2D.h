e2b25531ba7f6afa737cb9d3506c15507373532d
CSELCAR <cselcar@alumno.upv.es@ldsic-vdi07.upvnet.upv.es>
2025-11-20 09:24:27 +0100

    Añadida declaración de la clase Point2D

---
+++ Point2D.h
@@ -0,0 +1,37 @@
+#ifndef POINT2D_H
+#define POINT2D_H
+
+#include <ostream>
+#include <cmath>
+
+class Point2D{
+
+public:
+	//Atributos públicos
+	double x;
+	double y;
+
+	//Constructor con valores por defecto
+	Point2D(double x = 0, double y = 0);
+	
+	static double distance(const Point2D &a, const Point2D &b){
+		return std::sqrt(std::pow( a.x - b.x, 2) * std::pow(a.y - b.y, 2) );
+	}
+	
+	friend bool operator==(const Point2D &a, const Point2D &b){
+    		return (a.x == b.x) && (a.y == b.y);
+	}
+	
+	friend bool operator!=(const Point2D &a, const Point2D &b){
+                return (a.x != b.x) && (a.y != b.y);
+        }
+
+
+	friend std::ostream& operator<<(std::ostream &out, const Point2D &p){
+		out << "(" << p.x << ", " << p.y << ")";
+    		return out;
+	}
+
+};
+
+#endif
