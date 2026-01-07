d7207d35cf038cc0f5239f935e18107a8aa75d5a
CSELCAR <cselcar@alumno.upv.es@ldsic-vdi18.upvnet.upv.es>
2026-01-07 14:13:12 +0100

    Depuración Node

---
+++ testNode.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "node.h"
+using namespace std;
+int main(){
+	Node<char>* first = new Node<char>('A');
+	first= new Node<char>('R',first);
+	first= new Node<char>('P', first);
+	cout<<"Secuencia: ";
+	Node<char>*aux = first;
+	while(aux!=nullptr){
+		cout << aux->data << " ";
+		aux = aux->next;
+	}
+	cout << endl;
+	return 0;
+}
